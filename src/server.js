5a3fea3295414710581317e77c9fee660b0db2de
gabrieldk <ganriel201@gmail.com>
2021-07-09 17:39:05 -0300

    server port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 require("express-async-errors");
@@ -12,12 +14,14 @@ var app = express_1.default();
 app.use(express_1.default.json());
 app.use(routes_1.router);
 app.use(function (err, req, res, next) {
-    if (err instanceof Error) {
-        return res.status(400).json({ error: err.message });
-    }
-    return res.status(500).json({
-        status: "error",
-        message: "internal Server Error",
-    });
+  if (err instanceof Error) {
+    return res.status(400).json({ error: err.message });
+  }
+  return res.status(500).json({
+    status: "error",
+    message: "internal Server Error",
+  });
 });
-app.listen(3000, function () { return console.log("Server is running on port 3000"); });
+app.listen(process.env.PORT || 3000, function () {
+  return console.log("Server is running on port 3000");
+});
--- src/server.ts
+++ src/server.ts
@@ -24,4 +24,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Server is running on port 3000")
+);

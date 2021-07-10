c6b5062f1ba9a0bcb83d692f119b50a9cf108eb0
gabrieldk <ganriel201@gmail.com>
2021-07-10 20:32:06 -0300

    dist

--- dist/ormconfig.js
+++ dist/ormconfig.js
@@ -1,11 +1,12 @@
+"use strict";
 require("dotenv").config();
 module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
   synchronize: true,
   logging: false,
-  entities: ["src/entities/*.js"],
-  migrations: ["src/database/migrations/*.js"],
+  entities: ["src/entities/*.ts"],
+  migrations: ["src/database/migrations/*.ts"],
   cli: {
     entitiesDir: "src/entities",
     migrationsDir: "src/database/migrations",

292cc90a39f5d0eeee05172840241a8ee92b930c
gabrieldk <ganriel201@gmail.com>
2021-07-10 22:07:24 -0300

    teste

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 module.exports = {
   type: "postgres",
-  url: "postgres://mlwarphwlittum:e9ce8b200886a6c1f699f4a72317455159633f3ea1f0fc7ed978df8ce0f5b6f3@ec2-3-226-134-153.compute-1.amazonaws.com:5432/d2o6luhsa02dqd",
+  url: process.env.DATABASE_URL,
   synchronize: true,
   logging: false,
   entities: ["src/entities/*.js"],

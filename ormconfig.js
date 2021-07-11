f93b148a53d96678f7fd0c06c8bfa6aa46b43eb0
gabrieldk <ganriel201@gmail.com>
2021-07-11 09:01:23 -0300

    ssl config

--- ormconfig.js
+++ ormconfig.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
 module.exports = {
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    // url: process.env.DATABASE_URL,
-    entities: ["src/entities/*.js"],
-    migrations: ["src/database/migrations/*.js"],
-    cli: {
-        entitiesDir: "src/entities",
-        migrationsDir: "src/database/migrations",
-    },
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  ssl: true,
+  // url: process.env.DATABASE_URL,
+  entities: ["src/entities/*.js"],
+  migrations: ["src/database/migrations/*.js"],
+  cli: {
+    entitiesDir: "src/entities",
+    migrationsDir: "src/database/migrations",
+  },
 };

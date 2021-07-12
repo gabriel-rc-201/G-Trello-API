9b563eb78e27eb4907e1184cb2091360b19dc567
gabrieldk <ganriel201@gmail.com>
2021-07-12 16:25:23 -0300

    database credentials

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 module.exports = {
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_DATABASE,
   ssl: { rejectUnauthorized: false },
-  // url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL,
   entities: ["src/entities/*.js"],
   migrations: ["src/database/migrations/*.js"],
   cli: {
